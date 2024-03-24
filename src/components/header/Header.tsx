bc1d27a2d5740cf93b2e78d0e7a2185b5b2cf229
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-25 00:28:54 +0600

    padding changed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header:React.FC = () => {
   return (
-    <div className=' text-black bg-white p-10 dark:text-white '>
+    <div className=' p-10 text-black bg-white dark:text-white '>
         <div className=' container '>Home Route from dev branch </div>
     </div>
   )
