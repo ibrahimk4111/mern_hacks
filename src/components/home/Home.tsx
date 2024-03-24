3b67c5d30849100faf2068284b2cfc25301d472f
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-25 00:04:27 +0600

    dev branch

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home:React.FC = () => {
   return (
-    <div className=' container text-3xl font-bold text-center text-black bg-white p-10 dark:text-white '>Home </div>
+    <div className=' container text-3xl font-bold text-center text-black bg-white p-10 dark:text-white '>Home Route from dev branch </div>
   )
 }
 
